477a1358a6e1c07aec183afb6b42d8999ec9e19e
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-07-22 11:42:43 -0400

    Refactored get_files() and renamed as get_files_dirs(). Removed repetition of basepath. Cleaned up to be PEP8 compliant.

--- move_screen_caps/move_screen_shots.py
+++ move_screen_caps/move_screen_shots.py
@@ -6,22 +6,36 @@ import shutil
 import time
 
 
-def get_files(basepath):
-    # get file list and extract dates to check for directories
-    dirs = set()
-    for entry in os.listdir(basepath):
-        if entry.startswith('Screen Shot'):
-            folder = folder_date(basepath, entry)
-            dirs.add(folder)
-            print(entry)
-    return dirs
+def get_files_dirs(basepath: str):
+    '''
+    Get contents of desktop (basepath).
+    Return:
+        screen_shots: List of Screen Shots to be moved to directories
+                      based on capture date.
+        dirs: Set of required directories based on screen shot dates.
+    '''
+    path_contents = os.listdir(basepath)
+    desktop_contents = [os.path.join(basepath, file) for file in path_contents]
+    screen_shots = [file for file in desktop_contents if os.path.isfile(file)
+                    and 'Screen Shot' in file]
+    dirs = {folder_date(file) for file in screen_shots}
+    return screen_shots, dirs
 
-def folder_date(basepath, entry):
-    created = os.stat(basepath + entry).st_mtime
+
+def folder_date(file: list):
+    '''
+    Get folder dates based on file created dates.
+    Return:
+        date in YYYYMMDD format
+    '''
+    created = os.stat(file).st_mtime
     return time.strftime('%Y%m%d', time.localtime(created))
 
-def make_folders(dirs):
-    # check if directories exist and mkdir if not
+
+def make_folders(dirs: set):
+    '''
+    Check if directories exist and mkdir if not.
+    '''
     for dir in dirs:
         dir = destpath + dir
         file = pathlib.Path(dir)
@@ -31,24 +45,25 @@ def make_folders(dirs):
             os.mkdir(dir)
             print(dir, 'created.')
 
-def move_files(basepath, destpath):
-    # move files to directories
-    for entry in os.listdir(basepath):
-        if entry.startswith('Screen Shot'):
-            folder = folder_date(basepath, entry)
-            try:
-                source = basepath + entry
-                destination = destpath + folder + '/'
-                shutil.move(source, destination)
-            except Exception as e:
-                print(f'File not moved: {entry}. {e}')
-            else:
-                print(entry, 'moved.')
+
+def move_files(screen_shots: list, destpath: str):
+    '''
+    Move files to directories based on file created date.
+    '''
+    for file in screen_shots:
+        folder = folder_date(file)
+        try:
+            destination = destpath + folder + '/'
+            shutil.move(file, destination)
+        except Exception as e:
+            print(f'File not moved: {file}.\n{e}')
+        else:
+            print(file, 'moved.')
 
 
 if __name__ == '__main__':
     basepath = '/Users/username/Desktop/'
     destpath = '/Users/username/Documents/ScrShots_Desktop/'
-    dirs = get_files(basepath)
+    screen_shots, dirs = get_files_dirs(basepath)
     make_folders(dirs)
-    move_files(basepath, destpath)
+    move_files(screen_shots, destpath)
